7070c0974eb813d6a4422c0517bded201c70e29e
brian <liar88828@gmail.com>
2023-01-25 14:22:51 +0700

    7 sorting and formating

--- src/components/columns.jsx
+++ src/components/columns.jsx
@@ -1,8 +1,12 @@
+import {format} from "date-fns";
+
 export const COLUMNS = [
 	{Header: 'Id', Footer: 'Id', accessor: "id"},
 	{Header: 'First Name', Footer: 'First Name', accessor: "first_name"},
 	{Header: 'Last Name', Footer: 'Last Name', accessor: "last_name"},
-	{Header: 'Date of Birth', Footer: 'Date of Birth', accessor: "date_of_birth"},
+	{Header: 'Date of Birth', Footer: 'Date of Birth', accessor: "date_of_birth",
+		Cell:({value})=> {return	format( new Date(value), 'dd/MM/yyyy')}
+	},
 	{Header: 'Country', Footer: 'Country', accessor: "country"},
 	{Header: 'Phone', Footer: 'Phone', accessor: "phone"},
 ]
